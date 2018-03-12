0b8555b7821159fcc48ba5cca3453bce98b09057
SallyTemple <pfende@gmail.com>
2018-03-11 22:43:34 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,8 @@
       function foodGifs() {
 
             var topic = $(this).attr("data-name");
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+           
             // Clear all image
             $("#restaurants").empty();
 
