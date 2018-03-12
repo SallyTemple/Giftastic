82ce3383bf347212ae343ef8d0b256b80b7a2fe6
SallyTemple <pfende@gmail.com>
2018-03-12 00:27:46 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,67 @@
+var topics = ["Bacon", "Coffee", "Banana", "Burrito", "Ice Cream", "Pizza", "Salad", "Steak", "Taco", "Vodka"];
 
+// Render HTML to display the appropiate content
+function foodGifs() {
 
-      //Array of Food and Drink
-      var topics = ["Bacon", "Coffee", "Banana", "Cake", "Burrito", "Hot Dog", "Ice Cream", "Pizza", "Salad", "Steak", "Taco", "French Fries", "Vodka"];
+      var topic = $(this).attr("data-name");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      // Render HTML to display the appropiate content
-      function foodGifs() {
+      // Clear all image
+      $("#restaurants").empty();
 
-            var topic = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
-           
-            // Clear all image
-            $("#restaurants").empty();
+      // Creating an AJAX call for food and drink being clicked
+      $.ajax({
+            url: queryURL,
+            method: "GET"
+      }).then(function (response) {
+            var outcome = response.data;
+            console.log("response");
+            for (var i = 0; i < outcome.length; i++) {
+                  var yum = $("<img>");
+                  yum.attr("data-still", outcome[i].images.fixed_height_still.url);
+                  yum.attr("data-animate", outcome[i].images.fixed_height.url);
+                  yum.attr("src", outcome[i].images.fixed_height_still.url);
+                  yum.attr("data-state", "still");
+                  yum.attr("class", "snippet");
+                  var rating = $("<p>").text("Rated: " + outcome[i].rating);
 
-            // Creating an AJAX call for food and drink being clicked
-            $.ajax({
-                  url: queryURL,
-                  method: "GET"
-            }).then(function (response) {
-                  var outcome = response.data;
-                  for (var i = 0; i < outcome.length; i++) {
-                        var yum = $("<img");
-                        yum.attr("data-still", outcome[i].images.fixed_height_still.url);
-                        yum.attr("data-animate", outcome[i].images.fixed_height.url);
-                        yum.attr("src", outcome[i].images.fixed_height_still.url);
-                        yum.attr("data-state", "still");
-                        yum.attr("class", "snippet");
-                        var rating = $("<p>").text("Rated: " + outcome[i].rating);
-                       
-                        //Storing in each div
-                        var driveThru = $("<div>").attr("id", topic + i);
-                        driveThru.addClass("diner");
-                        driveThru.prepend(rating);
-                        driveThru.prepend(yum);
-                        $("#divide").prepend(gifBox);  
+                  //Storing in each div
+                  var driveThru = $("<div>").attr("id", topic + i);
+                  driveThru.addClass("diner");
+                  driveThru.prepend(rating);
+                  driveThru.prepend(yum);
+                  $("#divide").prepend(driveThru);
+            }
+            $(".snippet").on("click", function () {
+                  var state = $(this).attr("data-state");
+                  if (state == "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                  } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+
                   }
-                  $(".snippet").on("click", function() {
-                        var state = $(this).attr("data-state");
-                        if (state == "still") {
-                            $(this).attr("src", $(this).attr("data-animate"));
-                            $(this).attr("data-state", "animate");
-                        } else {
-                            $(this).attr("src", $(this).attr("data-still"));
-                            $(this).attr("data-state", "still");
-                          
-                        }
-                    });
-                  //Creating a div to hold the foodanddrink div
-                  var foodandDrinkDiv = $("<div class='foodandDrinks'>");
-                
             });
-      }
+
+
+      });
+}
+
+
+$(document).ready(function () {
+      // This function handles events where a foodanddrink button is clicked
+      $("#add-foodanddrink").on("click", function() {
+            event.preventDefault();
+            // This line grabs the input from the textbox
+            var foodandDrinks = $("#foodanddrink-input").val().trim();
+            $("#foodanddrink-input").val("");
+            //Adding food and drink from the textbox to our array
+            topics.push(foodandDrinks);
+            // Process foodandDrink Array
+            buttonOne();
+            $(".foodandDrink-btn").on("click", foodGifs)
+      });
 
       //Function for displaying food and drink data
       function buttonOne() {
@@ -58,32 +70,19 @@
             //Looping through the array of food and drink
             for (var i = 0; i < topics.length; i++) {
                   //Generate button for each food and drink in the array
-                  var p = $("<button>");
+                  var pie = $("<button>");
                   //Adding a class to the food and drink button
-                  p.addClass("foodandDrink-btn");
+                  pie.attr("class", "foodandDrink-btn");
                   // Adding a data attribute
-                  p.attr("data-name", topics[i]);
+                  pie.attr("data-name", topics[i]);
                   //Providing the intial button text
-                  p.text(topics[i]);
+                  pie.text(topics[i]);
                   //Adding the button to foodanddrink-button div
-                  $("#foodanddrink-button").append(p);
+                  pie.appendTo("#foodanddrink-button");
             }
       }
 
-      // This function handles events where a foodanddrink button is clicked
-      $("#add-foodanddrink").on("click", function (event) {
-            event.preventDefault();
-            // This line grabs the input from the textbox
-            var foodandDrinks = $("#foodanddrink-input").val().trim();
-            //Adding food and drink from the textbox to our array
-            topics.push(foodandDrinks);
-            // Process foodandDrink Array
-            buttonOne();
-            $(".foodandDrink-btn").on("click", foodGifs)
-      });
-
-     
-
-      // Calling the renderButtons function to display the intial buttons
+      // Calling the buttonOne function to display the intial buttons
       buttonOne();
       $(".foodandDrink-btn").on("click", foodGifs)
+});
